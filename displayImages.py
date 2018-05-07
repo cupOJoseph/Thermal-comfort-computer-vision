a862b80c8ad2ff8daf05e9a05a9b73a70af9fd70
Joseph Schiarizzi <jschiarizzi@gmail.com>
2018-05-06 18:42:25 -0700

    tried to detect blobs on faces

--- displayImages.py
+++ displayImages.py
@@ -12,10 +12,10 @@ thermal = "data/2018.01.22-19.41.50_11" #thermal
 kWidthIR = 160
 kHeightIR = 120
 def loadIR(filename):
-	array = np.fromfile(filename, dtype=np.uint16)
+	array = np.fromfile(filename, dtype=np.uint16)#try 8, messes with size of image
 	array = array.reshape((kHeightIR, kWidthIR))
 
-	array = cv2.normalize(array,0.0,1.0,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_32F)
+	array = cv2.normalize(array,0.0,1.0,norm_type=cv2.NORM_MINMAX,dtype=cv2.CV_32F)#try CV_8U
 	return cv2.flip(array, -1)
 
 kWidthRGB = 1280
@@ -30,8 +30,45 @@ def loadRGB(filename):
 colorImg = loadRGB(color)
 thermalImg = loadIR(thermal)
 
+#TODO Joseph Code
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 40
+params.maxThreshold = 150
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 1500
+
+# Filter by Circularity
+params.filterByCircularity = False
+params.minCircularity = 0.05
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.87
+
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+# Create a detector with the parameters
+ver = (cv2.__version__).split('.')
+if int(ver[0]) < 3 :
+	detector = cv2.SimpleBlobDetector(params)
+else :
+	detector = cv2.SimpleBlobDetector_create(params)
+
+
+# Detect blobs.
+im = cv2.imread(thermalImg, cv2.IMREAD_GRAYSCALE)
+keypoints = detector.detect(im)
+
+#===================== display
 
 cv2.imshow('color',colorImg)
 cv2.imshow('thermal',thermalImg)
 cv2.waitKey(0) #wait to press any key
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
---
+++ index.md
@@ -0,0 +1,2 @@
+# Goal
+Take a thermal image of the user and determine if they like the temperatureor not. 
